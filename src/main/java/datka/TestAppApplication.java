80094a937628050f95c3c95cf4689dc2853476c0
Datka <mamatzhanova.datka@gmail.com>
2024-04-06 15:25:14 +0600

    added new endpoint

--- src/main/java/datka/TestAppApplication.java
+++ src/main/java/datka/TestAppApplication.java
@@ -2,6 +2,7 @@ package datka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class TestAppApplication {
@@ -9,6 +10,11 @@ public class TestAppApplication {
     public static void main(String[] args) {
         SpringApplication.run(TestAppApplication.class, args);
         System.out.println("Project is running...");
+    }
+
+    @GetMapping
+    public String greetings(){
+        return "index";
     }
 
 }
