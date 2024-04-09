37fa798e313c8d5678f259f5d2ce8a65efa0e94f
Datka <mamatzhanova.datka@gmail.com>
2024-04-09 16:23:00 +0600

    .

--- src/main/java/datka/TestAppApplication.java
+++ src/main/java/datka/TestAppApplication.java
@@ -3,18 +3,21 @@ package datka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class TestAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TestAppApplication.class, args);
         System.out.println("Project is running...");
     }
-
-    @GetMapping
-    public String greetings(){
-        return "index";
+    @GetMapping("/api/test/hello")
+    public String hello(@RequestParam(required = false, defaultValue = "Datka") String name) {
+        return "Hello, %s!!!".formatted(name);
     }
+
 
 }
